b8d5f361de521f65c9f74a14886b2c135913d24e
domingao <49574878+domingao@users.noreply.github.com>
2019-06-27 20:13:53 -0300

    adicionado a classe string

---
+++ prog/Luanproj/src/main/java/String/Strings.java
@@ -0,0 +1,16 @@
+package String;
+
+public class Strings {
+	public static String botaoSalvar="Salvar";
+	public static String botaoLogar="Logar";
+	public static String botaoSair="Sair";
+	public static String botaoVoltar="Voltar";
+	public static String botaoprodutos="Produtos";
+	public static String botaoFornecedores="Fornecedores";
+	public static String botaoClientes="Clientes";
+	public static String labelLogin="NordicStream ♫";
+	public static String InfoUser = "Digite o Usuario :";
+	public static String InfoSenha = "Digite a Senha :";
+	
+
+}
